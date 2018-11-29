2d52c4783d107f7ab432de717d1bca8c855261ca
Pandey <vaibhav.pandey@wmeng.com>
2018-11-28 20:53:43 -0800

    added object constructor

---
+++ assets/server.js
@@ -0,0 +1,12 @@
+// creating object constructor
+function Guest(name, phone, email, uniqueID ){
+    this.name = name;
+    this.phone = phone;
+    this.email = email;
+    this.uniqueID = uniqueID;
+}
+// creating array of guests
+let guests = new Guest
+function reserveTable(){
+
+}
